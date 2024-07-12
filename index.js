5f954642417c03da41a8797701bca870b95f5e17
agustinFlw <164230624+agustinFlw@users.noreply.github.com>
2024-07-12 19:11:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+//import puppeteer from "puppeteer";
+
+//variables
+
+//inicio sesion:
+// elevenlabs
+
+
+function ingreso(){
+    //elevenlabs
+    let elevenLabsUser= document.getElementById('elevenLabsUser').value;
+    let elevenLabsPassword= document.getElementById('elevenLabsPassword').value;
+    //tiktok
+    let tiktokUser= document.getElementById('tiktokUser').value;
+    let tiktokPassword= document.getElementById('tiktokPassword').value;
+    //text
+    let story= document.getElementById('').value
+    console.log(elevenLabsUser);
+    console.log(elevenLabsPassword);
+    console.log(tiktokUser);
+    console.log(tiktokPassword);
+
+}
